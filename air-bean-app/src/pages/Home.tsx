36d47de7b358fd3ad33ace9fcaffc971e56a198c
Emadenni <emanyele.dng86@gmail.com>
2024-03-01 21:40:40 +0100

    created components and tested the fetch feature

--- air-bean-app/src/pages/Home.tsx
+++ air-bean-app/src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
 
+function Home() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(' https://airbean-api-xjlcn.ondigitalocean.app/api/beans/');
+        const jsonData = await response.json();
+        /* console.log(JSON.stringify(jsonData, null, 2)); */
+        console.log(jsonData);
+        
+        
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-const Home = () => {
-  return <div>Home</div>;
-};
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+     Home
+    </div>
+  );
+}
 
 export default Home;
