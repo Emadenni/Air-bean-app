8100e5d57ad7ead896d64a4ce8c94d739cdf3eec
Emadenni <emanyele.dng86@gmail.com>
2024-03-03 00:55:58 +0100

    adjusted footer/header style in menu and about pages

--- air-bean-app/src/components/Header/Header.tsx
+++ air-bean-app/src/components/Header/Header.tsx
@@ -1,10 +1,21 @@
 import top from "../../assets/images/top.png";
 import "../Header/header.scss";
+import navicon from "../../assets/images/navicon.svg";
+import carticon from "../../assets/images/carticon.png";
 
 const Header = () => {
   return (
     <>
       <img src={top} alt="top" />
+
+      <div className="headerIcons">
+        <img src={navicon} alt="headerIcons__navicon" />
+
+        <div className="cart-icons-wrapper">
+          <img src={carticon} alt="headerIcons__carticon" />
+          <p className="headerIcons__cart-counter">0</p>
+        </div>
+      </div>
     </>
   );
 };
