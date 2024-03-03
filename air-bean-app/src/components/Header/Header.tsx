7b2048bc2e5a827504ae7efd2f555484db60a61c
Emadenni <emanyele.dng86@gmail.com>
2024-03-03 14:13:55 +0100

    removed carticon from about page

--- air-bean-app/src/components/Header/Header.tsx
+++ air-bean-app/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import top from "../../assets/images/top.png";
 import navicon from "../../assets/images/navicon.svg";
 import carticon from "../../assets/images/carticon.png";
@@ -6,6 +7,8 @@ import "../Header/header.scss";
 import Nav from '../Nav/Nav';
 
 const Header = () => {
+  const location = useLocation();
+  const isAboutPage = location.pathname === '/about';
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleMenuToggle = () => {
@@ -18,11 +21,12 @@ const Header = () => {
 
     <div className="headerIcons">
       <img src={navicon} alt="headerIcons__navicon" onClick={handleMenuToggle} />
-
+      {!isAboutPage && (
       <div className="cart-icons-wrapper ">
         <img src={carticon} alt="headerIcons__carticon" />
         <p className="headerIcons__cart-counter">0</p>
       </div>
+       )}
     </div>
 
     {isMenuOpen && <Nav />}
