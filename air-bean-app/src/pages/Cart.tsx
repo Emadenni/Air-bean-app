d72d2a0eea4892edd6985e3a1be8b5935326ca55
Emadenni <emanyele.dng86@gmail.com>
2024-03-04 18:17:41 +0100

    updated cart's style

--- air-bean-app/src/pages/Cart.tsx
+++ air-bean-app/src/pages/Cart.tsx
@@ -2,82 +2,68 @@ import "../pages/cart.scss";
 import arrowUp from "../assets/images/arrow-up.svg";
 import vectorUp from "../assets/images/vector-up.svg";
 import vectorDown from "../assets/images/vector-down.svg";
-import { CartProps } from '../components/types';
+import { CartProps } from "../components/types";
 
 const Cart: React.FC<CartProps> = ({ handleToggleCart }: CartProps) => {
   return (
     <div className="cart-container">
       <img src={arrowUp} alt="arrow up" className="arrow-up" />
 
-      <ul className="cart-container__list">
+      <div className="cart-container__list">
         <h1>Din beställning</h1>
-         {/* da dinamicizzare */}
-        <li className="cart-container__item">
-          <p className="cart-container__item-p">
-            Bryggkaffe
-            <span>98 kr</span>
+        <ul>
+          <li className="cart-container__item">
+            <p className="cart-container__item-p">
+              Bryggkaffe
+              <span>98 kr</span>
+            </p>
+            <p className="cart-container__item-dots">..................................</p>
+
+            <div className="cart-container__quantityDiv">
+              <img src={vectorUp} alt="vector" />
+              <p>2</p>
+              <img src={vectorDown} alt="vector" />
+            </div>
+          </li>
+          <li className="cart-container__item">
+            <p className="cart-container__item-p">
+              Bryggkaffe
+              <span>98 kr</span>
+            </p>
+            <p className="cart-container__item-dots">..................................</p>
+
+            <div className="cart-container__quantityDiv">
+              <img src={vectorUp} alt="vector" />
+              <p>2</p>
+              <img src={vectorDown} alt="vector" />
+            </div>
+          </li>
+          <li className="cart-container__item">
+            <p className="cart-container__item-p">
+              Bryggkaffe
+              <span>98 kr</span>
+            </p>
+            <p className="cart-container__item-dots">..................................</p>
+
+            <div className="cart-container__quantityDiv">
+              <img src={vectorUp} alt="vector" />
+              <p>2</p>
+              <img src={vectorDown} alt="vector" />
+            </div>
+          </li>
+
+          {/* da dinamicizzare */}
+        </ul>
+
+        <div className="cart-container-total">
+          <p className="cart-container-total__text">
+            Total
+            <span>inkl moms + drönarleverans</span>
           </p>
-          <p className="cart-container__item-dots">..................................</p>
-
-          <div className="cart-container__quantityDiv">
-            <img src={vectorUp} alt="vector" />
-            <p>2</p>
-            <img src={vectorDown} alt="vector" />
-          </div>
-
-        </li>
-        <li className="cart-container__item">
-          <p className="cart-container__item-p">
-            Bryggkaffe
-            <span>98 kr</span>
-          </p>
-          <p className="cart-container__item-dots">..................................</p>
-
-          <div className="cart-container__quantityDiv">
-            <img src={vectorUp} alt="vector" />
-            <p>2</p>
-            <img src={vectorDown} alt="vector" />
-          </div>
-
-        </li>
-        <li className="cart-container__item">
-          <p className="cart-container__item-p">
-            Bryggkaffe
-            <span>98 kr</span>
-          </p>
-          <p className="cart-container__item-dots">..................................</p>
-
-          <div className="cart-container__quantityDiv">
-            <img src={vectorUp} alt="vector" />
-            <p>2</p>
-            <img src={vectorDown} alt="vector" />
-          </div>
-
-        </li>
-        <li className="cart-container__item">
-          <p className="cart-container__item-p">
-            Bryggkaffe
-            <span>98 kr</span>
-          </p>
-          <p className="cart-container__item-dots">..................................</p>
-
-          <div className="cart-container__quantityDiv">
-            <img src={vectorUp} alt="vector" />
-            <p>2</p>
-            <img src={vectorDown} alt="vector" />
-          </div>
-
-        </li>
-
-        {/* da dinamicizzare */}
-      </ul>
-
-      <div>
-        <p className="cart-container__item-p">
-            Bryggkaffe
-            <span>98 kr</span>
-          </p>
-          <p className="cart-container__item-dots">..................................</p> </div>
+          <p className="cart-container-total__dots">...............................</p>{" "}
+          <p className="cart-container-total__price">343 kr</p>
+        </div>
+      </div>
     </div>
   );
 };
