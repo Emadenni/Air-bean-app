abac7cee0c702d1d55661f906fcae114e0aaa838
Emadenni <emanyele.dng86@gmail.com>
2024-03-08 14:47:53 +0100

    styled page Status

--- air-bean-app/src/pages/Status.tsx
+++ air-bean-app/src/pages/Status.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import droneImg from "../assets/images/droneImg.svg"
+import "../pages/status.scss"
 
 const Status = () => {
   const [eta, setEta] = useState("");
@@ -21,13 +22,13 @@ const Status = () => {
   
 
   return (
-    <div>
-       <p>Order Number: {orderNr}</p>
+    <div className="status-container">
+       <p className="status-container__orderNr">Ordernummer  <span>#{orderNr}</span> </p>
        <img src={droneImg} alt="drone" />
   
-      <p>Din beställning är på väg!</p>
-      <p>ETA: {eta} <span>minuter</span> </p> 
-     <button>Ok, cool</button>
+      <p className="status-container__text" >Din beställning är på väg!</p>
+      <p className="status-container__eta">{eta} <span>minuter</span> </p> 
+     <button className="status-container__button">Ok, cool</button>
     </div>
   );
 };
